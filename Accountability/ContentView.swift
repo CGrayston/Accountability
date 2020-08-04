4c08b5414db041a9e94f9989df0ab2c8bd78fb86
CGrayston <chris.grayston95@gmail.com>
2020-08-03 22:11:07 -0700

    Initial Commit

---
+++ Accountability/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accountability
+//
+//  Created by Christopher Grayston on 8/3/20.
+//  Copyright © 2020 Christopher Grayston. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
